bc0bb8027775537bcc8c9bb848745d9d770fc9b7
Alice <hifumialice@gmail.com>
2024-05-15 23:22:24 +0900

    date 타입 String에서 LocalDateTime으로 수정

--- src/main/kotlin/com/teamsparta/todolist/domain/todo/dto/TodoResponse.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/todo/dto/TodoResponse.kt
@@ -1,9 +1,14 @@
 package com.teamsparta.todolist.domain.todo.dto
 
+import org.springframework.format.annotation.DateTimeFormat
+import java.time.LocalDateTime
+
 
 data class TodoResponse (
+    val id : Long,
     val title : String,
     val content : String,
-    val date : String,
-    val name : String
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    val date : LocalDateTime,
+    val writer : String
 )
