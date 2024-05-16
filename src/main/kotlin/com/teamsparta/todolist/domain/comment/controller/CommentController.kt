77869f1c112af9b03e7e4f8d180661bc440d3305
Alice <hifumialice@gmail.com>
2024-05-16 20:46:38 +0900

    body부분 구현

--- src/main/kotlin/com/teamsparta/todolist/domain/comment/controller/CommentController.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/comment/controller/CommentController.kt
@@ -3,29 +3,31 @@ package com.teamsparta.todolist.domain.comment.controller
 import com.teamsparta.todolist.domain.comment.dto.CommentCreateRequest
 import com.teamsparta.todolist.domain.comment.dto.CommentResponse
 import com.teamsparta.todolist.domain.comment.dto.CommentUpdateRequest
+import com.teamsparta.todolist.domain.comment.service.CommentService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 
 @RequestMapping("/todos/{id}/")
 @RestController
-class CommentController {
+class CommentController(private val commentService : CommentService) {
 
     @PostMapping()
     fun createComment(@PathVariable id : Long, @RequestBody request : CommentCreateRequest) : ResponseEntity<CommentResponse> {
-        TODO("Not yet Implemented")
+        return ResponseEntity.status(HttpStatus.CREATED).body(commentService.createComment(id, request))
     }
 
     @PutMapping("/{comment-id}")
     fun updateComment(@PathVariable id : Long, @PathVariable(name = "comment-id") commentId : Long,
                       @RequestBody request : CommentUpdateRequest)
     : ResponseEntity<CommentResponse> {
-        TODO("Not yet Implemented")
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.updateComment(id, commentId, request ))
     }
 
     @DeleteMapping("/{comment-id}")
     fun deleteComment(@PathVariable id : Long, @PathVariable(name = "comment-id") commentId : Long) : ResponseEntity<Unit> {
-        TODO("Not yet Implemented")
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.deleteComment(id, commentId))
     }
 
 }
