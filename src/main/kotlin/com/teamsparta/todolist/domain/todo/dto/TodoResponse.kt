2c47f25e8fcbbaeeabfa5d1ec9a412feebdbc12b
Alice <hifumialice@gmail.com>
2024-05-16 11:18:06 +0900

    어노테이션 수정

--- src/main/kotlin/com/teamsparta/todolist/domain/todo/dto/TodoResponse.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/todo/dto/TodoResponse.kt
@@ -1,5 +1,6 @@
 package com.teamsparta.todolist.domain.todo.dto
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalDateTime
 
@@ -8,7 +9,7 @@ data class TodoResponse (
     val id : Long,
     val title : String,
     val content : String,
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     val date : LocalDateTime,
     val writer : String
 )
