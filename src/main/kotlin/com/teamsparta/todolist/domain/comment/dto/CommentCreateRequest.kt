233fe4175bbdd67cf7b98ffe2650863872db3185
Alice <hifumialice@gmail.com>
2024-05-17 12:22:30 +0900

    property 수정

--- src/main/kotlin/com/teamsparta/todolist/domain/comment/dto/CommentCreateRequest.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/comment/dto/CommentCreateRequest.kt
@@ -2,6 +2,6 @@ package com.teamsparta.todolist.domain.comment.dto
 
 data class CommentCreateRequest (
     val comment : String,
-    val commentWriter : String,
+    val writer : String,
     val password : String
 )
