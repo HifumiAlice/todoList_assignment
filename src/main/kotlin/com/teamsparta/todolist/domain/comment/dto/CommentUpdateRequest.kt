1d35e686b264bf630c8cee960d92dcf758f49cd3
Alice <hifumialice@gmail.com>
2024-05-17 12:47:22 +0900

    property update

--- src/main/kotlin/com/teamsparta/todolist/domain/comment/dto/CommentUpdateRequest.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/comment/dto/CommentUpdateRequest.kt
@@ -2,6 +2,6 @@ package com.teamsparta.todolist.domain.comment.dto
 
 data class CommentUpdateRequest (
     val comment : String,
-    val commentWriter : String,
+    val writer : String,
     val password : String
 )
