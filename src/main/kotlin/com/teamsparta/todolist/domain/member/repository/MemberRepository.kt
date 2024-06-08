4e343ba7eca993e1b6b168523599bd02dca91166
Alice <hifumialice@gmail.com>
2024-06-08 17:08:54 +0900

    Add find one member by memberId

--- src/main/kotlin/com/teamsparta/todolist/domain/member/repository/MemberRepository.kt
+++ src/main/kotlin/com/teamsparta/todolist/domain/member/repository/MemberRepository.kt
@@ -2,8 +2,11 @@ package com.teamsparta.todolist.domain.member.repository
 
 import com.teamsparta.todolist.domain.member.model.Member
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface MemberRepository : JpaRepository<Member, Long> {
     fun existsByAccount(account: String): Boolean
 
+    @Query("select m from Member m where m.id = :id")
+    fun findByIdOne(id : Long): Member
 }
